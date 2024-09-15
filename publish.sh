e4b5119bd8a5f6c0e2d9395abc0177ee1d1a5fc8
Jessie Micallef <jessiem@mit.edu>
2024-09-15 16:24:16 -0400

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -1,24 +1,31 @@
 #!/bin/sh
 set -e
 
+# Configuration
 INPUT_BRANCH=${INPUT_BRANCH:-gh-pages}
 INPUT_DIRECTORY=${INPUT_DIRECTORY:-'website/build/tech-portfolio'}
+TEMP_DIR=$(mktemp -d)  # Create a temporary directory
 
-echo "Push to branch $INPUT_BRANCH";
+# Check for GITHUB_TOKEN
 [ -z "${GITHUB_TOKEN}" ] && {
     echo 'Missing input "GITHUB_TOKEN=${{ secrets.GITHUB_TOKEN }}".';
     exit 1;
 };
 
-cp -r ${INPUT_DIRECTORY} /gh-pages
+# Copy build files to the temporary directory
+cp -r ${INPUT_DIRECTORY}/* ${TEMP_DIR}
 
-cd /gh-pages
+# Navigate to the temporary directory
+cd ${TEMP_DIR}
 git init
 git config --local user.email "action@github.com"
 git config --local user.name "GitHub Action"
 git add .
-git commit -m "Push to Github Pages"
+git commit -m "Push to GitHub Pages"
 
+# Push to the gh-pages branch
 remote_repo="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
-
 git push "${remote_repo}" HEAD:${INPUT_BRANCH} --follow-tags --force
+
+# Clean up
+rm -rf ${TEMP_DIR}
